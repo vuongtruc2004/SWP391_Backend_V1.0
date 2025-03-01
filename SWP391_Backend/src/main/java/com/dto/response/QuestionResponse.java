7c0f4fccc9c2c33c51668e1d7c0399cf77bc6f7b
borisdoo-1304 <cuongdxhe186323@fpt.edu.vn>
2025-03-01 21:24:45 +0700

    them ham trong questionressponse

--- SWP391_Backend/src/main/java/com/dto/response/QuestionResponse.java
+++ SWP391_Backend/src/main/java/com/dto/response/QuestionResponse.java
@@ -7,6 +7,8 @@ import lombok.experimental.FieldDefaults;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Builder
 @AllArgsConstructor
@@ -23,4 +25,15 @@ public class QuestionResponse {
     Instant createdAt;
     Instant updatedAt;
     Instant latestUpdate;
+    public Instant getLatestUpdate() {
+        Instant questionUpdatedAt = Optional.ofNullable(updatedAt).orElse(Instant.MIN);
+        Instant answerUpdatedAt = answers != null
+                ? answers.stream()
+                .map(AnswerEntity::getUpdatedAt)
+                .filter(Objects::nonNull)
+                .max(Instant::compareTo)
+                .orElse(Instant.MIN)
+                : Instant.MIN;
+        return questionUpdatedAt.isAfter(answerUpdatedAt) ? questionUpdatedAt : answerUpdatedAt;
+    }
 }
