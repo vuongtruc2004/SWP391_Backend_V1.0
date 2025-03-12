40219d7685b82204649df611dcd217028e9a6888
ducnhhe186325 <ducnhhe186325@fpt.edu.vn>
2025-03-12 13:43:39 +0700

    update websocket

--- SWP391_Backend/src/main/java/com/configuration/CouponConfig.java
+++ SWP391_Backend/src/main/java/com/configuration/CouponConfig.java
@@ -1,7 +1,9 @@
 package com.configuration;
 import com.entity.CouponEntity;
 import com.repository.CouponRepository;
+import com.service.CouponService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,7 +15,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CouponConfig {
     private final CouponRepository couponRepository;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
+    public void deleteCouponSuccess(){
+        simpMessagingTemplate.convertAndSend("/topic/coupon","Delete Success!");
+    }
     @Async
     @Scheduled(fixedRate = 60000)
     @Transactional
@@ -23,10 +29,12 @@ public class CouponConfig {
         List<CouponEntity> endCoupons=this.couponRepository.findAll();
         if (!expiredCoupons.isEmpty()) {
             couponRepository.deleteAll(expiredCoupons);
+            deleteCouponSuccess();
         }
         for(CouponEntity coupon : endCoupons) {
             if(coupon.getMaxUses() == coupon.getUsedCount()){
                 this.couponRepository.deleteById(coupon.getCouponId());
+                deleteCouponSuccess();
             }
         }
     }
